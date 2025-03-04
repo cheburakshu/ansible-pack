658bdf72e71c5b525b2ef7704d83e48700d999db
cheburakshu <rajagopalasu@vmware.com>
2025-03-04 15:42:03 -0700

    include saltenv in cp.cache_file

--- src/ansiblepack/salt/_modules/ansiblepack.py
+++ src/ansiblepack/salt/_modules/ansiblepack.py
@@ -1,6 +1,6 @@
 # noqa: INP001
 def module(mod_name, saltenv="base", **params):
-    path = __salt__["cp.cache_file"](f"salt://{mod_name}.zip")  # noqa: F821
+    path = __salt__["cp.cache_file"](f"salt://{mod_name}.zip", saltenv=saltenv)  # noqa: F821
     if not path:
         return f"Unable to find {mod_name} in {saltenv}."
     return __utils__["ansiblepack.run"](mod_name, path, params)  # noqa: F821
